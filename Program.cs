36949038e4afe6838207ce2985c6d9d2756e3bc3
C:\Users\duce\AppData\Roaming\The Bat! <steinkrauz@yahoo.com>
2012-04-27 00:53:52 +0400

    Graphical syscalls added

--- Program.cs
+++ Program.cs
@@ -5,6 +5,27 @@ using System.Windows.Forms;
 
 namespace qdvm {
     class Program {
+        static ScrForm frm;
+        static int DrawStar(QDVMthread thr){
+            int y = thr.st.Pop();
+            int x = thr.st.Pop();
+            int stage = thr.st.Pop();
+            frm.DrawPix3(x,y,stage);
+            frm.Refresh();
+            return 0;
+        }
+        static int GetBounds(QDVMthread thr){
+            thr.st.Push(Screen.PrimaryScreen.Bounds.Width);
+            thr.st.Push(Screen.PrimaryScreen.Bounds.Height);
+            return 0;
+        }
+        static int EraseStar(QDVMthread thr)
+        {
+            int y = thr.st.Pop();
+            int x = thr.st.Pop();
+            frm.ErasePix(x, y);
+            return 0;
+        }
         static void Main(string[] args) {
             byte mode = 0;
             bool needLog = false;
@@ -41,10 +62,13 @@ namespace qdvm {
 
             StkQDVM VM = new StkQDVM();
             VM.Init(baseDir);
+            StkQDVM.Syscalls[31] = DrawStar;
+            StkQDVM.Syscalls[32] = GetBounds;
+            StkQDVM.Syscalls[33] = EraseStar;
             if (runGUI) {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                ScrForm frm = new ScrForm(Screen.PrimaryScreen.Bounds);
+                frm = new ScrForm(Screen.PrimaryScreen.Bounds);
                 frm.Show();
             }
             try {
